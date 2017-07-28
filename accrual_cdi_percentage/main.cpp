9a9c8dc0895f8852b4cf1f74df7ca5e75a9bd75e
Hugo Corra <hugocorra@gmail.com>
2017-07-28 08:53:52 -0300

    initial implementation of a class intented to calculate CDI accrual

--- accrual_cdi_percentage/main.cpp
+++ accrual_cdi_percentage/main.cpp
@@ -4,6 +4,66 @@
 
 using namespace QuantLib;
 
+namespace QuantLib {
+
+    //! %Coupon paying a fixed interest rate
+    class OvernightRateCoupon : public Coupon {
+      public:
+        //! \name constructors
+        //@{
+        OvernightRateCoupon(const Date& paymentDate,
+                            Real nominal,
+                            const std::vector<InterestRate>& interestRates,
+                            Spread spread,
+                            const Date& accrualStartDate,
+                            const Date& accrualEndDate,
+                            const Date& refPeriodStart,
+                            const Date& refPeriodEnd,
+                            const Date& exCouponDate)
+        : Coupon(paymentDate, nominal, accrualStartDate, accrualEndDate,
+             refPeriodStart, refPeriodEnd, exCouponDate),
+          rates_(interestRates),
+          spread_(spread)
+        {
+        }
+
+        Rate rate() const
+        {
+            return 0.0;
+        }
+
+        DayCounter dayCounter() const
+        {
+            return Business252();
+        }
+
+        Real accruedAmount(const Date&) const
+        {
+            return 0.0; //Date::todaysDate();
+        }
+
+        Real amount() const {
+            //auto brl_calendar = Brazil();
+            auto future_value = nominal();
+    
+            //for //(auto d = accrualStartDate_; d <= accrualEndDate; d = brl_calendar.advance(d, 1, Days, Following))
+            for (const auto rate : rates_)
+            {
+                future_value *= ((rate.compoundFactor(1/252.) - 1) * spread_) + 1;
+            }
+
+            return future_value;
+        }
+
+      private:
+        std::vector<InterestRate> rates_;
+        Spread spread_;
+    };
+};
+
+
+
+
 int main() {
     auto cdi_percentage = 0.85;
 
@@ -37,6 +97,12 @@ int main() {
 //        std::cout << "accrued = " << loan.accruedAmount(payment_date) << std::endl;
 //        */
     }
+
+    std::vector<InterestRate> vec_irates;
+    std::transform(rates.begin(), rates.end(), std::back_inserter(vec_irates), InterestRateCDI);
+    
+    OvernightRateCoupon cdi(today, loan_amount, vec_irates, cdi_percentage, today, today, today, today, today);
+    std::cout << "my class cdi.amount() is " << std::setprecision(9) << cdi.amount() << std::endl;
     
     std::cout << "future_value = " << std::setprecision(9) << future_value << std::endl;  
 
